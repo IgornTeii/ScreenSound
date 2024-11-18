14ad9978ac201bcd1acf08960196bf56b14ab5e5
Igor <igornteixeira48@gmail.com>
2024-11-17 21:07:46 -0300

    feat: menus

---
+++ Menus/MenuSair.cs
@@ -0,0 +1,11 @@
+﻿using ScreenSound_POO.Models;
+
+namespace ScreenSound_POO.Menus;
+
+internal class MenuSair : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.WriteLine("Tchau tchau :)");
+    }
+}
